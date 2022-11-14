2a536d8ec1101fdb9e3626ab47fa8f90957f7ad9
bomingeem <bomingeem@gmail.com>
2022-11-14 17:01:26 +0900

    싱글톤 패턴 4부 - 안전하고 단순하게 구현하는 방법: enum 타입을 쓸 경우 리플렉션 사용 불가하게끔 처리

--- src/main/java/designpatterns/singleton/Settings.java
+++ src/main/java/designpatterns/singleton/Settings.java
@@ -1,28 +1,5 @@
 package designpatterns.singleton;
 
-import java.io.Serializable;
-
-public class Settings implements Serializable {
-
-    private Settings() {
-    }
-
-    private static class SettingsHolder {
-        private static final Settings INSTANCE = new Settings();
-    }
-
-    /**
-     * 1. synchronized 사용
-     * 2. 이른 초기화(eager initialization) 사용
-     * 3. double-checked locking 사용
-     * 4. static inner 클래스 사용 (권장하는 방법 중 하나)
-     */
-    public static Settings getInstance() {
-        return SettingsHolder.INSTANCE;
-    }
-
-    //직렬화 대응 방안
-    protected Object readResolve() {
-        return getInstance();
-    }
+public enum Settings {
+    INSTANCE;
 }
