6e6e705dd0a14dcb7d60a54580ababeb463db543
bomingeem <bomingeem@gmail.com>
2022-11-14 16:59:41 +0900

    싱글톤 패턴 3부 - 싱글톤 패턴 구현 방법을 깨트리는 방법: 리플렉션 사용하기

--- src/main/java/designpatterns/singleton/App.java
+++ src/main/java/designpatterns/singleton/App.java
@@ -1,8 +1,17 @@
 package designpatterns.singleton;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
 public class App {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
         Settings settings = Settings.getInstance();
-        System.out.println(settings == Settings.getInstance());
+
+        //리플렉션 사용하기
+        Constructor<Settings> constructor = Settings.class.getDeclaredConstructor();
+        constructor.setAccessible(true);
+        Settings settings1 = constructor.newInstance();
+
+        System.out.println(settings == settings1);
     }
 }
