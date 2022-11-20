9519e5e90013d59b0f37831380a3be2ef9787918
bomingeem <bomingeem@gmail.com>
2022-11-20 15:11:09 +0900

    플라이웨이트 패턴 2부 - 패턴 적용하기

---
+++ src/main/java/designpatterns/flyweight/step2/Font.java
@@ -0,0 +1,19 @@
+package designpatterns.flyweight.step2;
+
+public final class Font {
+    final String fontFamily;
+    final int fontSize;
+
+    public Font(String fontFamily, int fontSize) {
+        this.fontFamily = fontFamily;
+        this.fontSize = fontSize;
+    }
+
+    public String getFontFamily() {
+        return fontFamily;
+    }
+
+    public int getFontSize() {
+        return fontSize;
+    }
+}
