e38e2098c4433f5973c1db913f9bae8882951d08
bomingeem <bomingeem@gmail.com>
2022-11-22 00:13:18 +0900

    책임 연쇄 패턴 4부 - 자바와 스프링에서 찾아보는 패턴

---
+++ src/main/java/designpatterns/chainofresponsibility/step3/App.java
@@ -0,0 +1,13 @@
+package designpatterns.chainofresponsibility.step3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+
+@ServletComponentScan
+@SpringBootApplication
+public class App {
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+}
