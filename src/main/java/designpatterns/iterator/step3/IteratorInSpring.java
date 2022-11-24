04a6d71c41e19e739742f4dfac0fe9adfd3bca27
bomingeem <bomingeem@gmail.com>
2022-11-25 00:23:02 +0900

    이터레이터 패턴 4부 - 자바와 스프링에서 찾아보는 패턴

---
+++ src/main/java/designpatterns/iterator/step3/IteratorInSpring.java
@@ -0,0 +1,9 @@
+package designpatterns.iterator.step3;
+
+import org.springframework.util.CompositeIterator;
+
+public class IteratorInSpring {
+    public static void main(String[] args) {
+        CompositeIterator iterator;
+    }
+}
