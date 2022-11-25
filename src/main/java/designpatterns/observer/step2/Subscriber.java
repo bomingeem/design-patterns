6b8725adf7ead93190eff7ea1e77e8a4effd4069
bomingeem <bomingeem@gmail.com>
2022-11-25 20:29:52 +0900

    옵저버 패턴 2부 - 패턴 적용하기

---
+++ src/main/java/designpatterns/observer/step2/Subscriber.java
@@ -0,0 +1,5 @@
+package designpatterns.observer.step2;
+
+public interface Subscriber {
+    void handleMessage(String message);
+}
---
+++ src/main/java/designpatterns/observer/step2/User.java
@@ -0,0 +1,18 @@
+package designpatterns.observer.step2;
+
+public class User implements Subscriber {
+    private String name;
+
+    public User(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public void handleMessage(String message) {
+        System.out.println(message);
+    }
+}
