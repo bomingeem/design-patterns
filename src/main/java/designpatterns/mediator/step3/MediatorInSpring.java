4a4e73c0ddabb38199b67dfbf2c0fdaa2e8fa79c
bomingeem <bomingeem@gmail.com>
2022-11-25 18:25:05 +0900

    중재자 패턴 4부 - 자바와 스프링에서 찾아보는 패턴

---
+++ src/main/java/designpatterns/mediator/step3/MediatorInSpring.java
@@ -0,0 +1,9 @@
+package designpatterns.mediator.step3;
+
+import org.springframework.web.servlet.DispatcherServlet;
+
+public class MediatorInSpring {
+    public static void main(String[] args) {
+        DispatcherServlet dispatcherServlet;
+    }
+}
