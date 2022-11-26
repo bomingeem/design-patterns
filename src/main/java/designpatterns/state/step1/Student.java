75bffa4d2fcec39da247b6222510ac8b5d420025
bomingeem <bomingeem@gmail.com>
2022-11-26 13:42:39 +0900

    상태 패턴 1부 - 패턴 소개

---
+++ src/main/java/designpatterns/state/step1/Student.java
@@ -0,0 +1,29 @@
+package designpatterns.state.step1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Student {
+    private String name;
+
+    public Student(String name) {
+        this.name = name;
+    }
+
+    private List<OnlineCourse> privateCourses = new ArrayList<>();
+
+    public boolean isEnabledForPrivateClass(OnlineCourse onlineCourse) {
+        return privateCourses.contains(onlineCourse);
+    }
+
+    public void addPrivateCourse(OnlineCourse onlineCourse) {
+        this.privateCourses.add(onlineCourse);
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+}
